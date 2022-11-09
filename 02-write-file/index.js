5e0ca374e6ba45b91052bdc6e44b89d0a7f461c3
sohodoll <kovalevvtl@gmail.com>
2022-11-09 13:18:01 +0300

    fix: fix exit in 02 and substring in 05

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,25 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const { stdin, stdout } = process;
 
-const writeStream = fs.createWriteStream(path.resolve(__dirname, 'text.txt'));
+const writeStream = fs.createWriteStream(path.resolve(__dirname, "text.txt"));
 
-stdout.write('Write something to add to the text file!\n');
+stdout.write("Write something to add to the text file!\n");
 
-stdin.on('data', data => {
-    if (data.toString() == 'exit\n') {
-        stdout.write('\nThanks, GL & HF!\n');
+stdin.on("data", (data) => {
+    if (data.toString().trim() == "exit") {
+        stdout.write("\nThanks, GL & HF!\n");
         process.exit();
     }
     writeStream.write(data);
-    stdout.write('Written! Anything else?\n');
+    stdout.write("Written! Anything else?\n");
 });
 
-process.on('SIGINT', () => {
-    stdout.write('\nThanks, GL & HF!\n');
+process.on("SIGINT", () => {
+    stdout.write("\nThanks, GL & HF!\n");
     process.exit();
-})
-
-
-
-
+});
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,37 +1,39 @@
-const fs = require('fs');
-const fsPromise = require('fs').promises;
-const path = require('path');
+const fs = require("fs");
+const fsPromise = require("fs").promises;
+const path = require("path");
 
-const stylesDir = path.resolve(__dirname, 'styles' );
-const writeStream = fs.createWriteStream(path.resolve(__dirname, 'project-dist/bundle.css' ));
+const stylesDir = path.resolve(__dirname, "styles");
+const writeStream = fs.createWriteStream(
+    path.resolve(__dirname, "project-dist/bundle.css")
+);
 let stylesArr = [];
-
 
 async function bundleStyles(dir) {
     await findFiles(dir);
 }
 
 async function findFiles(dir) {
-    const files = await fsPromise.readdir(dir, {withFileTypes: true});
-    console.log('These are the files inside this directory:')
+    const files = await fsPromise.readdir(dir, { withFileTypes: true });
+    console.log("These are the files inside this directory:");
     for (const file of files) {
-        if(!file.isDirectory()) {
-            let fileName = file.name.substr(0, file.name.lastIndexOf("."));
+        if (!file.isDirectory()) {
+            let fileName = file.name.substring(0, file.name.lastIndexOf("."));
             let fileExt = path.extname(file.name);
-            let fileSize = fs.stat(dir + '/' + file.name, (err, stats) => {
+            let fileSize = fs.stat(dir + "/" + file.name, (err, stats) => {
                 if (err) {
                     console.log(err);
                 } else {
-                    if (fileExt == '.css') {
-                        let readStream = fs.createReadStream(path.resolve(__dirname, `styles/${file.name}`));
+                    if (fileExt == ".css") {
+                        let readStream = fs.createReadStream(
+                            path.resolve(__dirname, `styles/${file.name}`)
+                        );
                         readStream.pipe(writeStream);
                         console.log(file.name);
                     }
                 }
             });
-        };
+        }
     }
 }
-
 
 bundleStyles(stylesDir);
